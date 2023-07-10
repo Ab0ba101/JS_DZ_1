b69212d358cfcbc59d163902970086416dbe916a
Ab0ba101 <timohapaskevic101@gmail.com>
2023-07-10 09:30:33 +0300

    First commit

---
+++ javascript.js
@@ -0,0 +1,60 @@
+// 1
+let age = 12;
+
+console.log(age);
+
+// 2
+let name = "Timofey";
+
+console.log(name);
+
+// 3
+
+let isStudent = "true";
+
+console.log(isStudent);
+
+// 4
+let myString = "Дія не завжди приведе до щастя, але до щастя привести може лише дія."
+
+console.log(myString)
+
+// 5
+let myNumber = 101;
+
+myNumber += 10;
+
+console.log(myNumber);
+
+// 6
+let myNull = "null";
+
+console.log(myNull);
+
+// 7
+let welcomeName = prompt("Введіть ваше ім'я");
+
+alert ("Привіт," + welcomeName);
+
+// 8
+let checkConfiguration = confirm("Підтвердіть дію.");
+
+if(checkConfiguration) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відміненно!")
+}
+
+// 9
+alert ("небезпечна дія!");
+
+let warning = confirm ("Будь ласка підтвердіть дію!");
+
+if(checkConfiguration) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відміненно!")
+}
+
+// END
+
